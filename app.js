af66a1e1a338c15796d7c5744d5fe80e1c3354fd
DmytroSokolovskyi <dim4uk@gmail.com>
2021-09-30 11:10:00 +0300

    Task 1 sort people

---
+++ app.js
@@ -0,0 +1,8 @@
+const sort = require('./helpers/people-sort');
+const path = require("path");
+
+const allPeoplesPath = path.join(__dirname, 'peoples')
+
+console.log(sort.sortPeople(allPeoplesPath));
+
+
---
+++ helpers/people-sort.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+
+const sortPeople = (mainDir) => {
+    fs.readdir(mainDir, (err, dirsGender) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        dirsGender.forEach(dirGender => {
+            fs.readdir(path.join(mainDir, dirGender), (e, files) => {
+                    if (e) {
+                        console.log(e);
+                        return;
+                    }
+                    files.forEach(file => {
+                        fs.readFile(path.join(mainDir, dirGender, file), (err1, data) => {
+                            if (e) {
+                                console.log(err1);
+                                return;
+                            }
+                            const item = JSON.parse(data);
+                            if (item.gender === dirGender) {
+                                return;
+                            }
+                            fs.rename(
+                                path.join(mainDir, dirGender, file),
+                                path.join(mainDir, item.gender, file),
+                                err => {
+                                    console.log(err)
+                                });
+                        })
+                    })
+                }
+            )
+        })
+
+    })
+};
+
+module.exports = {
+    sortPeople
+};
