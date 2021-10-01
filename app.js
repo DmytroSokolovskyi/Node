f79f6ce182fdf70aab8a2156b1e8701be16787a9
DmytroSokolovskyi <dim4uk@gmail.com>
2021-10-01 06:50:13 +0300

    CW-1

--- app.js
+++ app.js
@@ -1,53 +1,6 @@
+const sort = require('./helpers/people-sort');
 const path = require("path");
-const fs = require("fs");
 
-const dirs = ['manOlder20', 'manYounger20', 'womanOlder20', 'womanYounger20'];
+const allPeoplesPath = path.join(__dirname, 'peoples');
 
-dirs.forEach(dir => {
-    fs.mkdir(path.join(__dirname, dir), err => {
-        console.log(err)
-    });
-});
-
-const users = [
-    {name: 'Olya', gender: 'female', age: 20},
-    {name: 'Anya', gender: 'female', age: 18},
-    {name: 'Ira', gender: 'female', age: 19},
-    {name: 'Nadia', gender: 'female', age: 23},
-    {name: 'Sofia', gender: 'female', age: 24},
-    {name: 'Oleg', gender: 'male', age: 25},
-    {name: 'Vova', gender: 'male', age: 18},
-    {name: 'Max', gender: 'male', age: 23},
-    {name: 'Sacha', gender: 'male', age: 19},
-    {name: 'Kolya', gender: 'male', age: 20},
-];
-
-users.forEach(user => {
-
-    if (user.gender === "male") {
-
-        if (user.age >= 20) {
-            fs.writeFile(path.join(__dirname, 'manOlder20', `${user.name}.json`), JSON.stringify(user), err => {
-                console.log(err);
-            });
-            return;
-        }
-        fs.writeFile(path.join(__dirname, 'manYounger20', `${user.name}.json`), JSON.stringify(user), err => {
-            console.log(err)
-        })
-
-    }
-    if (user.age >= 20) {
-        fs.writeFile(path.join(__dirname, 'womanOlder20', `${user.name}.json`), JSON.stringify(user), err => {
-            console.log(err);
-        });
-        return;
-    }
-    fs.writeFile(path.join(__dirname, 'womanYounger20', `${user.name}.json`), JSON.stringify(user), err => {
-        console.log(err)
-    })
-
-});
-
-
-
+console.log(sort.sortPeople(allPeoplesPath));
--- helpers/people-sort.js
+++ helpers/people-sort.js
@@ -1,25 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-
 const sortPeople = (mainDir) => {
     fs.readdir(mainDir, (err, dirsGender) => {
+
         if (err) {
             console.log(err);
             return;
         }
+
         dirsGender.forEach(dirGender => {
             fs.readdir(path.join(mainDir, dirGender), (e, files) => {
+
                     if (e) {
                         console.log(e);
                         return;
                     }
+
                     files.forEach(file => {
                         fs.readFile(path.join(mainDir, dirGender, file), (err1, data) => {
+
                             if (e) {
                                 console.log(err1);
                                 return;
                             }
+
                             const item = JSON.parse(data);
                             if (item.gender === dirGender) {
                                 return;
@@ -30,12 +35,10 @@ const sortPeople = (mainDir) => {
                                 err => {
                                     console.log(err)
                                 });
-                        })
+                        });
                     })
-                }
-            )
-        })
-
+                });
+        });
     })
 };
 
