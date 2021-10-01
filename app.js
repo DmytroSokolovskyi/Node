8d1dd566d36f945c3ed1cd21c25758ab892419c5
DmytroSokolovskyi <dim4uk@gmail.com>
2021-10-01 07:55:15 +0300

    HW-1

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const sort = require('./helpers/people-sort');
+// const sort = require('./helpers/people-sort');
+const newSort = require('./helpers/newPeople-sort');
 const path = require("path");
 
-const allPeoplesPath = path.join(__dirname, 'peoples');
+// const allPeoplesPath = path.join(__dirname, 'peoples');
+const malePeoplesPath = path.join(__dirname, 'peoples', 'male');
+const femalePeoplesPath = path.join(__dirname, 'peoples', 'female');
 
-console.log(sort.sortPeople(allPeoplesPath));
+// console.log(sort.sortPeople(allPeoplesPath));
+console.log(newSort.newSortPeople(malePeoplesPath, malePeoplesPath, femalePeoplesPath));
+console.log(newSort.newSortPeople(femalePeoplesPath, malePeoplesPath, femalePeoplesPath));
+
 
 
 
---
+++ helpers/newPeople-sort.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+const newSortPeople = (toSortDir, maleDir, femaleDir) => {
+    fs.readdir(toSortDir, (e, files) => {
+
+        if (e) {
+            console.log(e);
+            return;
+        }
+
+        files.forEach(file => {
+            fs.readFile(path.join(toSortDir, file), (err, data) => {
+
+                if (e) {
+                    console.log(err);
+                    return;
+                }
+
+                const item = JSON.parse(data);
+                const writePath = item.gender === "male" ? maleDir : femaleDir;
+
+                fs.rename(
+                    path.join(toSortDir, file),
+                    path.join(writePath, file),
+                    err => {
+                        console.log(err);
+                    });
+            });
+        })
+    });
+};
+
+module.exports = {
+    newSortPeople
+};
