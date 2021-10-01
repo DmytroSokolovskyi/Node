d262112820b3a9e6bf4bd043754215f0fbd6164b
DmytroSokolovskyi <dim4uk@gmail.com>
2021-10-01 06:55:36 +0300

    master

--- app.js
+++
@@ -1,8 +0,0 @@
-const sort = require('./helpers/people-sort');
-const path = require("path");
-
-const allPeoplesPath = path.join(__dirname, 'peoples')
-
-console.log(sort.sortPeople(allPeoplesPath));
-
-
