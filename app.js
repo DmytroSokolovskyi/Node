67b05df804682af26a0318df976ab43858175b38
DmytroSokolovskyi <dim4uk@gmail.com>
2021-10-12 16:49:35 +0300

    HW-5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const {config} = require('./configs');
 const {userRouter, userAuthRouter} = require('./routes');
